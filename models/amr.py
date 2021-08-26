cb4ffab2602cc82c6d9478d7d7ede0a35ce6e3f8
Eloi <tanguy.eloi@gmail.com>
2021-08-26 15:00:08 +0200

    typos

--- models/amr.py
+++ models/amr.py
@@ -622,7 +622,7 @@ def get_most_recurrent_sub_path(sentences, pair_node_ids):
     all_sub_paths = {}
     for path in paths:
         for start_idx in range(1, len(path) - 1):
-            for end_idx in range(start_idx + 1, len(path) - 1):
+            for end_idx in range(start_idx + 1, len(path)):
                 sub_path = path[start_idx:end_idx]
                 simplified_sub_path = get_simplified_path_list(sub_path)
                 if str(simplified_sub_path) not in all_sub_paths:
@@ -633,7 +633,7 @@ def get_most_recurrent_sub_path(sentences, pair_node_ids):
     max_count, most_common_sub_path, length = 0, None, 0
     for d in all_sub_paths.values():
         if d['count'] > max_count or (d['count'] == max_count and len(d['sub_path']) > length):
-            max_count, most_common_sub_path = d['count'], d['sub_path']
+            max_count, most_common_sub_path, length = d['count'], d['sub_path'], len(d['sub_path'])
 
     return most_common_sub_path
 
